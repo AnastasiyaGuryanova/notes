11c132f7cb918485760ee608e0d394a698c3d736
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-23 13:24:10 +0300

    feat: добавлены ui компоненты Modal и Button

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Group, Modal as MantineModal } from '@mantine/core';
+import { Button } from 'shared/ui';
+import { ModalProps } from './Modal.types';
+
+export const Modal: FC<ModalProps> = ({ opened, onClose, onConfirm, message }) => {
+	return (
+		<MantineModal
+			opened={opened}
+			onClose={onClose}
+			title={message}
+			c="myColor.9"
+			styles={{
+				content: {
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					textAlign: 'center',
+				},
+				header: {
+					width: '100%',
+				},
+			}}
+		>
+			<Group mt="md">
+				<Button c="myColor.0" color="red.7" onClick={onConfirm}>
+					Удалить
+				</Button>
+			</Group>
+		</MantineModal>
+	);
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,4 @@
 export * from './ErrorBoundary';
 export * from './ErrorComponent';
+export * from './Button';
+export * from './Modal';
