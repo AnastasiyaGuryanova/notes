1d50db232be8cdd520d5e499ba90511c121bdf7f
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-23 12:48:38 +0300

    feat: добавлена шапка приложения и настроен главный компонент

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,42 @@
+import { AppShell, Burger, Group, Box, useMantineTheme } from '@mantine/core';
+import { LogoutButton } from 'features/auth';
+
+interface HeaderProps {
+	opened: boolean;
+	toggle: () => void;
+}
+
+export const Header = ({ opened, toggle }: HeaderProps) => {
+	const theme = useMantineTheme();
+
+	return (
+		<AppShell.Header
+			style={{
+				backgroundColor: theme.colors.myColor[9],
+			}}
+		>
+			<Box
+				style={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					height: '100%',
+					padding: 'md',
+				}}
+			>
+				<Burger
+					opened={opened}
+					onClick={toggle}
+					hiddenFrom="sm"
+					size="md"
+					color="myColor.0"
+					style={{ marginLeft: '20px' }}
+				/>
+
+				<Group justify="flex-end" style={{ margin: '0 20px 0 auto' }}>
+					<LogoutButton />
+				</Group>
+			</Box>
+		</AppShell.Header>
+	);
+};
