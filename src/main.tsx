b7ad33d5cd17d6f571f4d209e92352bd5bc08a44
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-05-01 16:58:03 +0300

    feat: настроен PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({
+	immediate: true,
+});
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
